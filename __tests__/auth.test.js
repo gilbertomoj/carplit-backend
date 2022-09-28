e76ed91b7efb8b57403c04cc1de0c1cc28ad36ec
Gilberto Medeiros <gibamedeirosgc@gmail.com>
2022-09-28 00:55:47 -0300

    user tests

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -3,17 +3,16 @@ const ApiUrl = "http://localhost:8080";
 let token = '';
 
 beforeAll(() => {
-  return request(ApiUrl)
-    .post("/user/login")
-    .send({
+    return request(ApiUrl)
+      .post("/user/login")
+      .send({
         email: "gaaga@gmail.com",
         password: "1234"
-    })
-    .expect(200)
-    .then(response => {
+      })
+      .expect(200)
+      .then(response => {
         expect(response.body.status).toEqual(200);
-        token = response.body.token;
-    });
+      });
 })
 
 describe("GET /user/login", () => {
