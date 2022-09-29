a32d89600da2c7d89553af667c72771bf5dadb23
thaisdka <thaisdekassia1@gmail.com>
2022-09-28 22:07:31 -0300

    organize tests

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,74 +1,78 @@
 const request = require("supertest");
 const ApiUrl = "http://localhost:8080";
-let token = '';
+let token = "";
 const obj = {
-    name: "José", email: "joseaa@gmail.com", password: "1234", average_consumption: 14, fuel_per_liter: 11.4
-}
+    name: "Carplit",
+    email: "test@carplitest.com",
+    password: "test@123",
+    average_consumption: 14,
+    fuel_per_liter: 11.4,
+};
+
 beforeAll(() => {
     return request(ApiUrl)
-      .post("/user/login")
-      .send({
-        email: "gaaga@gmail.com",
-        password: "1234"
-      })
-      .expect(200)
-      .then(response => {
-        expect(response.body.status).toEqual(200);
-      });
-})
-
-describe("GET /user/login", () => {
-  it("should return 200 and return the access token", () => {
-    return request(ApiUrl)
-      .post("/user/login")
-      .send({
-        email: "gaaga@gmail.com",
-        password: "1234"
-      })
-      .expect(200)
-      .then(response => {
-        expect(response.body.status).toEqual(200);
-      });
-  });
-});
-
-describe("POST user/verify/email", ()=>{
-    it("should return 400 and a error message", ()=>{
-        return request(ApiUrl)
-        .post("/user/verify/email")
-        .send({
-            email: "gaaga@gmail.com"
-    })
-    .then(response => {
-        expect(response.body.status).toEqual(400);
-    });
-    });
-})
-
-describe("POST user/register", ()=>{
-    it("should return 200 and a confirmation message", ()=>{
-        return request(ApiUrl)
-        .post("/user/register")
-        .send(obj)
-        .then(response => {
-            console.log(response.body)
-            expect(response.body.status).toEqual(200);
-        });
-    });
-
-})
-
-describe("POST user/login", ()=>{
-    it("should return 200 and a confirmation message", ()=>{
-        return request(ApiUrl)
         .post("/user/login")
         .send({
             email: "gaaga@gmail.com",
-            password: "1234"
+            password: "1234",
         })
-        .then(response => {
-            console.log(response.body)
+        .expect(200)
+        .then((response) => {
             expect(response.body.status).toEqual(200);
         });
+});
+
+describe("GET /user/login", () => {
+    it("should return 200 and return the access token", () => {
+        return request(ApiUrl)
+            .post("/user/login")
+            .send({
+                email: "gaaga@gmail.com",
+                password: "1234",
+            })
+            .expect(200)
+            .then((response) => {
+                expect(response.body.status).toEqual(200);
+            });
     });
-})
+});
+
+describe("POST user/verify/email", () => {
+    it("should return 400 and a error message", () => {
+        return request(ApiUrl)
+            .post("/user/verify/email")
+            .send({
+                email: "gaaga@gmail.com",
+            })
+            .then((response) => {
+                expect(response.body.status).toEqual(400);
+            });
+    });
+});
+
+describe("POST user/register", () => {
+    it("should return 200 and a confirmation message", () => {
+        return request(ApiUrl)
+            .post("/user/register")
+            .send(obj)
+            .then((response) => {
+                console.log(response.body);
+                expect(response.body.status).toEqual(200);
+            });
+    });
+});
+
+describe("POST user/login", () => {
+    it("should return 200 and a confirmation message", () => {
+        return request(ApiUrl)
+            .post("/user/login")
+            .send({
+                email: "test@carplit.com",
+                password: "test@123",
+            })
+            .then((response) => {
+                console.log(response.body);
+                expect(response.body.status).toEqual(200);
+            });
+    });
+});
